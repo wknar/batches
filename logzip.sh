16133a7f50f019a877d4401b2de30dfd57862026
wknar <wakana.ara@gmail.com>
2014-02-07 19:13:49 +0900

    added argument, but i have to fix some parts.

--- logzip.sh
+++ logzip.sh
@@ -1,14 +1,26 @@
+#! /usr/bin/env sh
 # compess log files created 1 week ago
 # delete them after more 1 week
 # file format -> yyyymmdd-hh:mm:log(refer to screenrc)
 
-DATE_C=`date -d '1 week ago' '+%Y%m%d'`
+# set data
+if [ $1 ]; then
+  if [[ ! $1 =~ "2[0-9]{3}[01][0-9][0123][0-9]$" ]]; then
+    echo "invalid argument. please write as YYYYMMDD"
+    exit 0
+  else
+    DATE_C=$1
+  fi
+else
+  DATE_C=`date -d '1 day ago' '+%Y%m%d'`
+fi
+
 DATE_D=`date -d '2 week ago' '+%Y%m%d'`
 
 # change file path for each env
 cd ~/log/local/
 
-if ls $DATE_c* > /dev/null 2>&1
+if ls $DATE_C* > /dev/null 2>&1
 then
   tar cvf $DATE_C $DATE_C*
   gzip $DATE_C
